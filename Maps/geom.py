5aafbd03af9965c098c64db8917b6b0514cf0d68
Alex Hinds <alex.hinds141@gmail.com>
2017-01-18 21:18:12 +1100

    Update geom.py

--- Maps/geom.py
+++ Maps/geom.py
@@ -2,6 +2,25 @@
 
 import random, triangle
 
+"""
+    Map generating code using voronoi polygons.
+    
+    Copyright (C) 2017 Alexander Walker Hinds
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
 class Point:
     """docstring for Point."""
     def __init__(self,x,y,ide=-1):
